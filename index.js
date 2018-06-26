7a50b783c7fbe76c0640535b7e9303a2a1f6857c
Kavitha Kamal <kavithakamal@gmail.com>
2018-06-26 14:55:06 -0400

    Added a endpoint in index.js

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ const express = require('express');
 const app = express();
 const pg = require('pg');
 
+app.use(express.logger());
 app.get('/', (req, res) => res.send('<h1><marquee>Re-Spec\'d!</marquee></h1>'));
 
-app.listen(process.env.PORT || 3000, 
-    () => console.log('listening on 3000!'));
+const port = process.env.PORT || 3000;
+    app.listen(port, function() {
+      console.log("Listening on " + port);
+    });
+    
+app.get('/register', function(request, response) {
+      console.log("Request Body",request); 
+      response.send('Hello World!'+request.body);
+    });
+   
+// pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//     client.query('SELECT * FROM GLASSES', function(err, result) {
+//         done();
+//         if(err) return console.error(err);
+//         console.log(result.rows);
+//       });
+// });    
