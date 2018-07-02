78f5ae97b699e14e207b24085274419319f6a72f
Peter Marsh <p.marsh@mac.com>
2018-07-02 12:55:02 -0400

    added dot json

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
   
 // client.connect();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.post('/register', user.registerUser);
 app.post('/login', login.loginUser);
--- respecd.sql
+++ respecd.sql
@@ -18,6 +18,7 @@ CREATE TABLE IF NOT EXISTS glasses(
   leftsphere NUMERIC (4, 2),
   rightsphere NUMERIC (4, 2),
   description VARCHAR(500),
+  rating INTEGER,
   location VARCHAR(50),
   image VARCHAR(500),
   userid INTEGER REFERENCES users(userid)
@@ -27,15 +28,9 @@ CREATE TABLE IF NOT EXISTS frames(
   frameid serial PRIMARY KEY,
   title VARCHAR(100) NOT NULL,
   description VARCHAR(500),
+  rating INTEGER,
   location VARCHAR(50),
   image VARCHAR(500),
   userid INTEGER REFERENCES users(userid)
 );
 
-insert into users(firstname ,lastname ,email ,password ) values ('kavitha','kamal','kavithakamal@gmail.com','pass');
-insert into users(firstname ,lastname ,email ,password ) values ('peter','marsh','ptrthegr8@gmail.com','pass');
-insert into users(firstname ,lastname ,email ,password ) values ('ryan','agard','jragard8@gmail.com','pass');
-
-insert into frames(title, description, location, image, userid) values ('aaa','bbbbbbbbbbbb','ccc','ddddd',1);
-
-insert into glasses(title, leftsphere, rightsphere, description, location, image, userid)values ('mytitle', 2, 2,'dfsdfsd','dsfdsfsd','dsds',1)
