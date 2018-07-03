7614528da05b1b1e71b28ac404544979dc60a1c3
Kavitha Kamal <kavithakamal@gmail.com>
2018-07-03 17:30:52 -0400

    Code for passport jwt added

--- index.js
+++ index.js
@@ -6,17 +6,13 @@ const frames = require("./controllers/frames");
 const glasses = require("./controllers/glasses");
 const login = require("./controllers/login");
 const user = require("./controllers/user");
+const passport = require('passport');
 app.use(cors());
 app.use(express.json());
 
-// const { Client } = require('pg');
-// const connectionString = process.env.DATABASE_URL || 'respecdlocal';
-// const client = new Client(connectionString);
-  //({"postgres://fbarrottvccelh:03b1c75531e1281c206135c2f4e6ba97d1f70db4ef5477d45c8f58fee483eae3@ec2-23-23-245-89.compute-1.amazonaws.com:5432/deumig2doadmmi?ssl=true",
-  //connectionString: process.env.DATABASE_URL,
-  // ssl: true,})
-  
-// client.connect();
+require('./controllers/passport')(passport);  
+app.use(passport.initialize());  
+
 
 const port = process.env.PORT || 3000;
 
@@ -24,7 +20,7 @@ app.post('/register', user.registerUser);
 app.post('/login', login.loginUser);
 app.get('/logout', login.logoutUser);
 
-app.get('/glasses', glasses.getGlasses);
+app.get('/glasses',passport.authenticate('jwt', { session: false }), glasses.getGlasses);
 app.get('/glasses/:glassId', glasses.getSingleGlass);
 app.post('/glasses', glasses.addGlass);
 app.put('/glasses/:glassId', glasses.updateGlass);
