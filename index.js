a3ba2b346bd40f1c9653b801c0f17178a528d7dc
Peter Marsh <p.marsh@mac.com>
2018-07-05 13:27:27 -0400

    no null this time

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const frames = require("./controllers/frames");
 const glasses = require("./controllers/glasses");
 const login = require("./controllers/login");
 const user = require("./controllers/user");
-const config = null;
+const config;
 process.env.NODE_ENV !== 'production' ? config = require('./config') : null;
 // image upload stuff
 const AWS = require("aws-sdk");
